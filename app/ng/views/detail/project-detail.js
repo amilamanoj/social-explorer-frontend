37198757f1c741f10a8963d3221a519ac5259fdd
Alex Hufnagel <ga68fid@mytum.de>
2016-06-27 12:22:49 +0200

    updated popup window for host info

--- app/ng/views/detail/project-detail.js
+++ app/ng/views/detail/project-detail.js
@@ -89,27 +89,47 @@ angular.module('myApp.projects')
             });
         };
 
-        $scope.lookAtProfile = function(ev, user) { //just a preview example for a pop up window
-            // Appending dialog to document.body to cover sidenav in docs app
-            var confirm = $mdDialog.prompt()
-                .title('Profile of host:')
-                .textContent('Name: ')
+        // $scope.lookAtProfile = function(ev, user) { //just a preview example for a pop up window
+        //     // Appending dialog to document.body to cover sidenav in docs app
+        //     var confirm = $mdDialog.prompt()
+        //         .title('Profile of host:')
+        //         .textContent('Name: ')
+        //
+        //         // .textContent('Name: "' + user.username + '"')
+        //         .placeholder('statement')
+        //         .ariaLabel('statement')
+        //         // .initialValue('your text there')
+        //         .targetEvent(ev)
+        //         .ok('Apply!')
+        //          .cancel('Back');
+        //     $mdDialog.show(confirm).then(function(result) {
+        //         $scope.status = 'You applied with statement ' + result + '.';
+        //         sendApplication(result);
+        //     },
+        //         function() {
+        //         $scope.status = 'You canceled.';
+        //     });
+        // };
 
-                // .textContent('Name: "' + user.username + '"')
-                .placeholder('statement')
-                .ariaLabel('statement')
-                // .initialValue('your text there')
-                .targetEvent(ev)
-                .ok('Apply!')
-                 .cancel('Back');
-            $mdDialog.show(confirm).then(function(result) {
-                $scope.status = 'You applied with statement ' + result + '.';
-                sendApplication(result);
-            },
-                function() {
-                $scope.status = 'You canceled.';
-            });
+
+
+
+        $scope.lookAtProfile = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('Profile of host:')
+                    .textContent('Here should be some information about the host')   //How to get Host info and not logged in user?
+                    .ariaLabel('Host Info')
+                    .ok('Back')
+                    .targetEvent(ev)
+            );
         };
+
 
         function sendApplication(statement){
             console.log("create application");
