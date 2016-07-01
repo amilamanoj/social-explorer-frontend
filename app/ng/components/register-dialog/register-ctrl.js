f11f26c4225f9e28bb8409b650d5c85abc886ba5
Alex Hufnagel <ga68fid@mytum.de>
2016-07-01 17:47:03 +0200

    fixed some issues & added pwd comparison (not working right now...)

--- app/ng/components/register-dialog/register-ctrl.js
+++ app/ng/components/register-dialog/register-ctrl.js
@@ -9,6 +9,7 @@ angular.module('myApp')
       
         $scope.register = register;
         $scope.cancel = cancel;
+        $scope.checkPass = checkPass;
 
 
 
@@ -26,5 +27,34 @@ angular.module('myApp')
 
         function cancel() {
             $mdDialog.cancel();
+        }
+
+        function checkPass() //doesnt work right now
+        {
+            //Store the password field objects into variables ...
+            var password = document.getElementById('password');
+            var passwordConfirm = document.getElementById('passwordConfirm');
+            //Store the Confimation Message Object ...
+            var message = document.getElementById('confirmMessage');
+            //Set the colors we will be using ...
+            var goodColor = "#66cc66";
+            var badColor = "#ff6666";
+            //Compare the values in the password field 
+            //and the confirmation field
+            if(password.value == passwordConfirm.value){
+                //The passwords match. 
+                //Set the color to the good color and inform
+                //the user that they have entered the correct password 
+                passwordConfirm.style.backgroundColor = goodColor;
+                message.style.color = goodColor;
+                message.innerHTML = "Passwords Match!"
+            }else{
+                //The passwords do not match.
+                //Set the color to the bad color and
+                //notify the user.
+                pass2.style.backgroundColor = badColor;
+                message.style.color = badColor;
+                message.innerHTML = "Passwords Do Not Match!"
+            }
         }
     });
