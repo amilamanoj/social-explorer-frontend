8439b1ee7f23a02d8483d4bafe1bc459aaf5cd83
Amila Silva <amilamanoj@78.198.eduroam.dynamic.rbg.tum.de>
2016-07-02 15:36:29 +0200

    country service fixes

--- app/ng/components/config/country-service.js
+++ app/ng/components/config/country-service.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.applications', [])
+angular.module('myApp.countries', [])
 
-    .factory('CountryService', function(BASEURL, $resource) {
+    .factory('CountryService', function() {
         return  {
             countries : [
             "Afghanistan",
