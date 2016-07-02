f0a5da587f03b2af88157ecf3f34047487ed5a36
Mathias Staudigl <ga67mob@mytum.de>
2016-07-02 16:01:33 +0200

    clean up the rating things

--- app/ng/views/rating/rating-write.js
+++ app/ng/views/rating/rating-write.js
@@ -80,7 +80,101 @@ app.controller('ProfileWriteRatingCtrl', function(shareDataServiceRating,$scope,
                 .hideDelay(3000)
         );
     }
+    $scope.click1 = function (param) {
+        console.log('Click(' + param + ')');
+    };
+
+    $scope.mouseHover1 = function (param) {
+        console.log('mouseHover(' + param + ')');
+        $scope.hoverRating1 = param;
+    };
+
+    $scope.mouseLeave1 = function (param) {
+        console.log('mouseLeave(' + param + ')');
+        $scope.hoverRating1 = param + '*';
+    };
+
+    $scope.click2 = function (param) {
+        console.log('Click');
+    };
+
+    $scope.mouseHover2 = function (param) {
+        console.log('mouseHover(' + param + ')');
+        $scope.hoverRating1 = param;
+    };
+
+    $scope.mouseLeave2 = function (param) {
+        console.log('mouseLeave(' + param + ')');
+        $scope.hoverRating2 = param + '*';
+    };
 
 
 
+});
+
+
+app.directive('starRating2', function () {
+    return {
+        scope: {
+            rating: '=',
+            maxRating: '@',
+            readOnly: '@',
+            click: "&",
+            mouseHover: "&",
+            mouseLeave: "&"
+        },
+        restrict: 'EA',
+        template:
+            "<div style='display: inline-block; margin: 0px; padding: 0px; cursor:pointer;' ng-repeat='idx in maxRatings track by $index'> \
+                                       <img ng-src='{{((hoverValue + _rating) <= $index) && \"http://www.codeproject.com/script/ratings/images/star-empty-lg.png\" || \"http://www.codeproject.com/script/ratings/images/star-fill-lg.png\"}}' \
+                    ng-Click='isolatedClick($index + 1)' \
+                    ng-mouseenter='isolatedMouseHover($index + 1)' \
+                    ng-mouseleave='isolatedMouseLeave($index + 1)'/> </div>",
+        compile: function (element, attrs) {
+            if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
+                attrs.maxRating = '5';
+            };
+        },
+        controller: function ($scope, $element, $attrs) {
+            $scope.maxRatings = [];
+
+            for (var i = 1; i <= $scope.maxRating; i++) {
+                $scope.maxRatings.push({});
+            };
+
+            $scope._rating = $scope.rating;
+
+            $scope.isolatedClick = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope.rating = $scope._rating = param;
+                $scope.hoverValue = 0;
+                $scope.click({
+                    param: param
+                });
+            };
+
+            $scope.isolatedMouseHover = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = 0;
+                $scope.hoverValue = param;
+                $scope.mouseHover({
+                    param: param
+                });
+            };
+
+            $scope.isolatedMouseLeave = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = $scope.rating;
+                $scope.hoverValue = 0;
+                $scope.mouseLeave({
+                    param: param
+                });
+            };
+
+
+        }
+    };
 });
