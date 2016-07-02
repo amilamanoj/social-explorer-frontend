8a5d41818b99a477d580bf7f6448e22a4bc4eaa5
Alex Hufnagel <ga68fid@mytum.de>
2016-07-02 10:35:12 +0200

    deleted edit button on profile details

--- app/ng/views/profile/overview/profile-overview.js
+++ app/ng/views/profile/overview/profile-overview.js
@@ -104,12 +104,6 @@ angular.module('myApp.profile')
                 })
             }
         
-        $scope.goToEdit = function(ev, user) {
-            console.log("going to edit profile");
-            $state.go('profile.settings', {'userId': user._id });
-            
-            
-        }
 
         $scope.cancel = function() {
             $mdDialog.cancel();
